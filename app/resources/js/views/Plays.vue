66b238128b2dd5777893ab3f62fac6f0ff439956
RodrigoBaltazar <rodrigobaltazarinf@gmail.com>
2021-01-19 11:28:11 -0300

    vue plays

---
+++ app/resources/js/views/Plays.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- {{message}} -->
+    {{ plays }}
+  </div>
+</template>
+
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Plays",
+  components: {
+    Plays,
+  },
+  data() {
+    return {
+      plays: null,
+    };
+  },
+  mounted() {
+    axios.get("http://localhost:8000/api/plays").then((response) => {
+      console.log(response);
+      this.plays = response.data;
+    });
+  },
+};
+
+</script>
